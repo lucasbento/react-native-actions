1051ae3c043b04555b8acd7b00404fd704cc3c34
Lucas Bento <lucas.bsilva@outlook.com>
2017-10-03 10:41:56 +0200

    Clean up & run only on RN project
    Fixes #3

--- packages/vscode-react-native-actions/src/index.js
+++ packages/vscode-react-native-actions/src/index.js
@@ -1,35 +1,41 @@
 import {
-  ExtensionContext,
   StatusBarAlignment,
   window,
-  StatusBarItem,
-  Selection,
-  workspace,
-  TextEditor,
   commands,
+  workspace,
 } from 'vscode';
+import isPackageDep from 'is-package-dep';
 import server from 'http';
 import io from 'socket.io';
 
 import config from '../config';
 
-export function activate(context) {
-  const app = server.createServer();
-  const socket = io(app);
+export const activate = (context) =>
+  isPackageDep('react-native', {
+    baseDir: workspace.rootPath,
+  })
+    .then((isRNProject) => {
+      if (!isRNProject) {
+        return;
+      }
 
-  app.listen(config.port);
-
-  const status = window.createStatusBarItem(StatusBarAlignment.Left, 100);
-  status.command = 'extension.reloadReactNative';
-  status.text = '$(repo-sync)';
-  context.subscriptions.push(status);
-  status.show();
-
-  const subscription = commands.registerCommand('extension.reloadReactNative', () => {
-    socket.emit('action', {
-      type: 'reload',
+      const app = server.createServer();
+      const socket = io(app);
+    
+      app.listen(config.port);
+    
+      const status = window.createStatusBarItem(StatusBarAlignment.Left, 100);
+      status.command = 'extension.reloadReactNative';
+      status.text = '$(repo-sync)';
+      status.tooltip = 'Reload React-Native App';
+      context.subscriptions.push(status);
+      status.show();
+    
+      const subscription = commands.registerCommand('extension.reloadReactNative', () => {
+        socket.emit('action', {
+          type: 'reload',
+        });
+      });
+    
+      context.subscriptions.push(subscription);
     });
-  });
-
-  context.subscriptions.push(subscription);
-}
