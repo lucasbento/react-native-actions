215ab1f7b1aa672f212c995b1f0467486abdda3a
Lucas Bento <lucas.bsilva@outlook.com>
2017-10-19 12:30:14 +0200

    Check for package.json downwards the project tree

--- packages/vscode-react-native-actions/src/index.js
+++ packages/vscode-react-native-actions/src/index.js
@@ -22,36 +22,36 @@ const ANDROID_COMMANDS = {
   openDevMenu: 'adb shell input keyevent 82',
 };
 
-export const activate = (context) =>
-  isPackageDep('react-native', {
+export const activate = (context) => {
+  const isRNProject = isPackageDep('react-native', {
     baseDir: workspace.rootPath,
-  })
-    .then((isRNProject) => {
-      let socket = null;
-      if (isRNProject) {
-        const app = server.createServer();
-        socket = io(app);
-  
-        app.listen(config.port);
+  });
+
+  let socket = null;
+  if (isRNProject) {
+    const app = server.createServer();
+    socket = io(app);
+
+    app.listen(config.port);
+  }
+
+  COMMANDS.forEach((command) => {
+    const subscription = commands.registerCommand(command.trigger, () => {
+      // Handle hotkey even if it's not an RN project so it doesn't
+      // throw an error on VSCode
+      if (!isRNProject) {
+        return;
       }
 
-      COMMANDS.forEach((command) => {
-        const subscription = commands.registerCommand(command.trigger, () => {
-          // Handle hotkey even if it's not an RN project so it doesn't
-          // throw an error on VSCode
-          if (!isRNProject) {
-            return;
-          }
-
-          // Emit action for iOS
-          socket.emit('action', {
-            type: command.action,
-          });
-
-          // Run command for Android
-          exec(ANDROID_COMMANDS[command.action]);
-        });
-
-        context.subscriptions.push(subscription);
+      // Emit action for iOS
+      socket.emit('action', {
+        type: command.action,
       });
+
+      // Run command for Android
+      exec(ANDROID_COMMANDS[command.action]);
     });
+
+    context.subscriptions.push(subscription);
+  });
+};
